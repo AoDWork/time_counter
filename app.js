df58361a941b61e901ad5f389a3bd5c611256301
AoDWork <88074725+AoDWork@users.noreply.github.com>
2023-05-15 15:03:59 +0300

    Refactor + fix 1.14 bug

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ function zapusk() {
             countDrobnoe = Math.round((countDrobnoe - 0.60) * 100) / 100;
         }
         if (Math.round(countDrobnoe * 100) / 100 < 0.60) {
-            input = countCeloe + parseInt(countDrobnoe * 100) / 100;
+            input = (countCeloe + parseInt(countDrobnoe * 100) / 100).toFixed(2);
         };
         document.getElementById(firstColumnPosition).innerText = input;
         tap.value = "";
@@ -146,7 +146,7 @@ function zapusk1() {
             countDrobnoe = Math.round((countDrobnoe - 0.60) * 100) / 100;
         }
         if (Math.round(countDrobnoe * 100) / 100 < 0.60) {
-            input = countCeloe + parseInt(countDrobnoe * 100) / 100;
+            input = (countCeloe + parseInt(countDrobnoe * 100) / 100).toFixed(2);
         };
         document.getElementById(secondColumnPosition).innerText = input;
         tap.value = "";
@@ -183,7 +183,7 @@ function zapusk2() {
         }
 
         if (Math.round(countDrobnoe * 100) / 100 < 0.60) {
-            input = countCeloe + parseInt(countDrobnoe * 100) / 100;
+            input = (countCeloe + parseInt(countDrobnoe * 100) / 100).toFixed(2);
         }
 
         document.getElementById(thirdColumnPosition).innerText = input;
@@ -256,113 +256,49 @@ function setZero() {
     }
 };
 
-// counter all results	
+// count summ of results
 function countAllResults() {
+    let arrMinutes = []
+    let counterHours = 0
 
-    let c0 = document.getElementById(80).innerText;
-    var c1 = document.getElementById(81).innerText;
-    var c2 = document.getElementById(82).innerText;
-    var c3 = document.getElementById(83).innerText;
-    var c4 = document.getElementById(84).innerText;
-    var c5 = document.getElementById(85).innerText;
-    var c6 = document.getElementById(86).innerText;
-    var c7 = document.getElementById(87).innerText;
-    var c8 = document.getElementById(88).innerText;
-    var c9 = document.getElementById(89).innerText;
-    var c10 = document.getElementById(90).innerText;
-    var c11 = document.getElementById(91).innerText;
-    var c12 = document.getElementById(92).innerText;
-    var c13 = document.getElementById(93).innerText;
-    var c14 = document.getElementById(94).innerText;
-    var c15 = document.getElementById(95).innerText;
-    var c16 = document.getElementById(96).innerText;
-    var c17 = document.getElementById(97).innerText;
-    var c18 = document.getElementById(98).innerText;
-    var c19 = document.getElementById(99).innerText;
-    var c20 = document.getElementById(101).innerText;
-    var c21 = document.getElementById(102).innerText;
-    var c22 = document.getElementById(103).innerText;
-    var c23 = document.getElementById(104).innerText;
-
-    var h = 0, m = 0;
-    var hs0 = parseInt(c0) * 3600;
-    var hs1 = parseInt(c1) * 3600;
-    var hs2 = parseInt(c2) * 3600;
-    var hs3 = parseInt(c3) * 3600;
-    var hs4 = parseInt(c4) * 3600;
-    var hs5 = parseInt(c5) * 3600;
-    var hs6 = parseInt(c6) * 3600;
-    var hs7 = parseInt(c7) * 3600;
-    var hs8 = parseInt(c8) * 3600;
-    var hs9 = parseInt(c9) * 3600;
-    var hs10 = parseInt(c10) * 3600;
-    var hs11 = parseInt(c11) * 3600;
-    var hs12 = parseInt(c12) * 3600;
-    var hs13 = parseInt(c13) * 3600;
-    var hs14 = parseInt(c14) * 3600;
-    var hs15 = parseInt(c15) * 3600;
-    var hs16 = parseInt(c16) * 3600;
-    var hs17 = parseInt(c17) * 3600;
-    var hs18 = parseInt(c18) * 3600;
-    var hs19 = parseInt(c19) * 3600;
-    var hs20 = parseInt(c20) * 3600;
-    var hs21 = parseInt(c21) * 3600;
-    var hs22 = parseInt(c22) * 3600;
-    var hs23 = parseInt(c23) * 3600;
-
-    var ms0 = (Math.round((c0 - parseInt(c0)) * 100)) * 60;
-    var ms1 = (Math.round((c1 - parseInt(c1)) * 100)) * 60;
-    var ms2 = (Math.round((c2 - parseInt(c2)) * 100)) * 60;
-    var ms3 = (Math.round((c3 - parseInt(c3)) * 100)) * 60;
-    var ms4 = (Math.round((c4 - parseInt(c4)) * 100)) * 60;
-    var ms5 = (Math.round((c5 - parseInt(c5)) * 100)) * 60;
-    var ms6 = (Math.round((c6 - parseInt(c6)) * 100)) * 60;
-    var ms7 = (Math.round((c7 - parseInt(c7)) * 100)) * 60;
-    var ms8 = (Math.round((c8 - parseInt(c8)) * 100)) * 60;
-    var ms9 = (Math.round((c9 - parseInt(c9)) * 100)) * 60;
-    var ms10 = (Math.round((c10 - parseInt(c10)) * 100)) * 60;
-    var ms11 = (Math.round((c11 - parseInt(c11)) * 100)) * 60;
-    var ms12 = (Math.round((c12 - parseInt(c12)) * 100)) * 60;
-    var ms13 = (Math.round((c13 - parseInt(c13)) * 100)) * 60;
-    var ms14 = (Math.round((c14 - parseInt(c14)) * 100)) * 60;
-    var ms15 = (Math.round((c15 - parseInt(c15)) * 100)) * 60;
-    var ms16 = (Math.round((c16 - parseInt(c16)) * 100)) * 60;
-    var ms17 = (Math.round((c17 - parseInt(c17)) * 100)) * 60;
-    var ms18 = (Math.round((c18 - parseInt(c18)) * 100)) * 60;
-    var ms19 = (Math.round((c19 - parseInt(c19)) * 100)) * 60;
-    var ms20 = (Math.round((c20 - parseInt(c20)) * 100)) * 60;
-    var ms21 = (Math.round((c21 - parseInt(c21)) * 100)) * 60;
-    var ms22 = (Math.round((c22 - parseInt(c22)) * 100)) * 60;
-    var ms23 = (Math.round((c23 - parseInt(c23)) * 100)) * 60;
-
-    var res = (hs0 + hs1 + hs2 + hs3 + hs4 + hs5 + hs6 + hs7 + hs8 + hs9 + hs10 + hs11 + hs12 + hs13 + hs14 + hs15 + hs16 + hs17 + hs18 + hs19 + hs20 + hs21 + hs22 + hs23) + (ms0 + ms1 + ms2 + ms3 + ms4 + ms5 + ms6 + ms7 + ms8 + ms9 + ms10 + ms11 + ms12 + ms13 + ms14 + ms15 + ms16 + ms17 + ms18 + ms19 + ms20 + ms21 + ms22 + ms23);
-
-    resultMin = (res / 60);
-    while (res >= 3600) {
-        h++;
-        res = res - 3600;
+    for (let i = 110; i <= 134; i++){ 
+        arrMinutes.push(document.getElementById(i).innerText)
     }
-    if (res < 3600) {
-        m = (res / 60) / 100;
-    }
-    result = Math.round((h + m) * 100) / 100;
-    document.getElementById('itog').innerText = result;
-    document.getElementById('itogMin').innerText = resultMin;
-    view();
-};
 
-// view results 
-var view = function () {
+    let summOfMinutes = arrMinutes.reduce((total, el) => {
+        return total + +el
+    }, 0).toFixed()
+
+    let summOfHours = summOfMinutes
+
+    while (summOfHours >= 60) {
+        counterHours++;
+        summOfHours = summOfHours - 60;
+    }
+
+
+    let totalHours = (counterHours +'.'+ summOfHours)
+    document.getElementById('itog').innerText = totalHours;
+    document.getElementById('itogMin').innerText = summOfMinutes;
+    
+    view(totalHours, summOfMinutes);
+}
+
+
+// show results 
+var view = function (totalHours, summOfMinutes) {
     var massageArea = document.getElementById("messageArea");
-    massageArea.innerText = "Hours : " + result;
+    massageArea.innerText = "Hours : " + totalHours;
     var massageAreaMin = document.getElementById("messageAreaMin");
-    massageAreaMin.innerText = "Min. : " + resultMin;
+    massageAreaMin.innerText = "Min. : " + summOfMinutes;
 
 }
 
+
 view();
 
-//Показ подсказки
+
+// show tooltip
 document.onmouseover = function (e) {
     var target = e.target;
 
@@ -401,9 +337,6 @@ document.onmouseout = function (e) {
 
 };
 
-const cell = (e) => {
-
-}
 
 
 // Features for integrate
